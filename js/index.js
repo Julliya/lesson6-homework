5df556669643184cb1d89f1011e1b675388f1b7d
Julli Babenko <julli.babenko@gmail.com>
2017-05-11 20:04:49 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var a = Number(prompt('Enter A'));
+var b = Number(prompt('Enter B'));
+var c = Number(prompt('Enter C'));
+
+var result = squareEq(a, b, c);
+document.write(result);
+
+function squareEq(a, b, c) {
+    
+    var d = ;
+    
+    Math.sqrt(d)
+    
+    if(d > 0) {
+        result = 'x1 = ' + d*(-b)
+        result+= 'x2 = '
+        
+    }
+    
+    return result;
