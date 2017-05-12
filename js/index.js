cc7fe5f85d5cc7e70de4a0850a285fe48ad3bb48
Julli Babenko <julli.babenko@gmail.com>
2017-05-12 07:14:29 +0000

    done

--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
 var a = Number(prompt('Enter A'));
+if(a==0){
+    alert("A can not be 0!");
+    var a = Number(prompt('Enter A'));
+}
 var b = Number(prompt('Enter B'));
 var c = Number(prompt('Enter C'));
 
-var result = squareEq(a, b, c);
-document.write(result);
+if (Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)) {
+     document.write('You incorrectly entered number! Try again.');
+}
+else{
+    var result = squareEq(a, b, c);
+    document.write(result);
+}
+
 
 function squareEq(a, b, c) {
     
-    var d = ;
-    
-    Math.sqrt(d)
+    var d = Math.pow(b, 2)-4*a*c;
+    var result;
     
     if(d > 0) {
-        result = 'x1 = ' + d*(-b)
-        result+= 'x2 = '
-        
+        result = 'x1 = ' + (-b - Math.sqrt(d)) / (2 * a); 
+        result += ' x2 = ' + (-b + Math.sqrt(d)) / (2 * a);
     }
-    
+    else if(d == 0){
+        result = 'x1,2 = ' + (-b/(2*a));
+    }
+    else{
+        result = 'no decisions';
+    }
     return result;
+}
---
+++ js/test.js
@@ -0,0 +1,6 @@
+var num = Number(prompt('A'));
+if (Number.isNaN(num)) {
+alert('Введіть число!')
+} else {
+alert('Ви ввели число: ' + num);
+}
